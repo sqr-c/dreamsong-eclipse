a2569a1d1a867d5ed65308e49db3478432d2e4e7
sqr-c <45774329+sqr-c@users.noreply.github.com>
2023-10-31 23:23:00 -0500

    Add files via upload

--- eclipse-get/install.sh
+++ eclipse-get/install.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
-
-echo "Install target \"eclipse\"? (Y/N)"
-read buf
-
-if [[ "$buf" != "N" && "$buf" != "n" ]]; then
-	cd eclipse/ && sh install.sh && cd ..
-fi
-
-echo "Install target \"eclipse-get\"? (Y/N)"
-read buf
-
-if [[ "$buf" != "N" && "$buf" != "n" ]]; then
-	cd eclipse-get/ && sh install.sh && cd ..
-fi
+cmake -DBUILD=i -B build/ && \
+make -C build/ && \
+sudo make install -C build/ && \
+echo "Success!"
---
+++ eclipse-get/make.sh
@@ -0,0 +1,3 @@
+cmake -B build/ && \
+make -C build/ && \
+echo "Success!"
